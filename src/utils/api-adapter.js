4c20575648a6a9b93f54b0c2adcd4e124367bc28
serhii.shramko <shramko.web@yahoo.com>
2020-08-12 19:40:03 +0300

    chore: add api adapter

---
+++ src/utils/api-adapter.js
@@ -0,0 +1,15 @@
+import cloneDeep from 'lodash/cloneDeep';
+
+export const adaptAPI = (apiResponse) => {
+  const response = cloneDeep(apiResponse);
+
+  return response.map((item) => ({
+    color: item.color,
+    description: item.description,
+    dueDate: item['due_date'],
+    id: item.id,
+    isArchived: item['is_archived'],
+    isFavorite: item['is_favorite'],
+    repeatingDays: item['repeating_days'],
+  }));
+};
